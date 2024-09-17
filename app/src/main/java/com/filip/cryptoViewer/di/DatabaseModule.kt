016e5b392c401051a48456d23a919378658018ac
Filip <filipus959@gmail.com>
2024-09-17 18:30:14 +0200

    reinstalled repositoryModule that will provide repository instead of database module

--- app/src/main/java/com/filip/cryptoViewer/di/DatabaseModule.kt
+++ app/src/main/java/com/filip/cryptoViewer/di/DatabaseModule.kt
@@ -6,9 +6,6 @@ import com.filip.cryptoViewer.data.local.dao.CoinChartDao
 import com.filip.cryptoViewer.data.local.dao.CoinDao
 import com.filip.cryptoViewer.data.local.dao.CoinDetailDao
 import com.filip.cryptoViewer.data.local.dao.CoinTickerItemDao
-import com.filip.cryptoViewer.data.remote.CoinPaprikaApi
-import com.filip.cryptoViewer.data.repository.implementation.CoinRepositoryImpl
-import com.filip.cryptoViewer.domain.repository.CoinRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -47,11 +44,5 @@ object DatabaseModule {
     @Singleton
     fun provideCoinTickerItemDao(appDatabase: AppDatabase): CoinTickerItemDao {
         return appDatabase.coinTickerItemDao()
-    }
-
-    @Provides
-    @Singleton
-    fun provideCoinRepository(coinService: CoinPaprikaApi, coinTickerItemDao: CoinTickerItemDao,coinDetailDao: CoinDetailDao,coinChartDao: CoinChartDao): CoinRepository {
-        return CoinRepositoryImpl(coinService, coinTickerItemDao,coinDetailDao,coinChartDao)
     }
 }
--- app/src/main/java/com/filip/cryptoViewer/di/RepositoryModule.kt
+++ app/src/main/java/com/filip/cryptoViewer/di/RepositoryModule.kt
@@ -1,30 +1,24 @@
-//
-//import dagger.Module
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object RepositoryModule {
-//
-////    @Provides
-////    @Singleton
-////    fun provideCoinLocalRepository(coinDao: CoinDao): CoinLocalRepository {
-////        return CoinLocalRepository(coinDao)
-////    }
-//
-////    @Provides
-////    @Singleton
-////    fun provideCoinRemoteRepository(coinService: CoinPaprikaApi, coinTickerItemDao: CoinTickerItemDao): CoinRepositoryImpl {
-////        return CoinRepositoryImpl(coinService, coinTickerItemDao)
-////    }
-//
-////    @Provides
-////    @Singleton
-////    fun provideCoinRepository(
-////        coinLocalRepository: CoinLocalRepository,
-////        coinRemoteRepository: CoinRepository
-////    ): CoinRepository {
-////        return CoinRepositoryImpl(coinLocalRepository, coinRemoteRepository)
-////    }
-//}
+package com.filip.cryptoViewer.di
+
+import com.filip.cryptoViewer.data.local.dao.CoinChartDao
+import com.filip.cryptoViewer.data.local.dao.CoinDetailDao
+import com.filip.cryptoViewer.data.local.dao.CoinTickerItemDao
+import com.filip.cryptoViewer.data.remote.CoinPaprikaApi
+import com.filip.cryptoViewer.data.repository.implementation.CoinRepositoryImpl
+import com.filip.cryptoViewer.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(coinService: CoinPaprikaApi, coinTickerItemDao: CoinTickerItemDao, coinDetailDao: CoinDetailDao, coinChartDao: CoinChartDao): CoinRepository {
+        return CoinRepositoryImpl(coinService, coinTickerItemDao,coinDetailDao,coinChartDao)
+    }
+}
